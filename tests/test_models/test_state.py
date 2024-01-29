f25066363ff789b44ed3dd1b90baea5f61f8a52f
MAHAMAT263 <m.ahmat@alustudent.com>
2024-01-29 02:39:39 -0800

    Aibnbn

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.state import State
 
 
@@ -10,10 +10,11 @@ class test_state(test_basemodel):
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "State"
+        self.name = "California"
         self.value = State
 
     def test_name3(self):
         """ """
         new = self.value()
+        new.name = "Arizona"
         self.assertEqual(type(new.name), str)
